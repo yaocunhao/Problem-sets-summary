790dcee4a45c249335331ec4133d03cfcfbcae3b
姚存浩 <997106826@qq.com>
2021-01-13 14:06:10 +0800

    排序更新

---
+++ Sort/sort/sort.h
@@ -0,0 +1,33 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+#include <stdio.h>
+#include <windows.h>
+#include <time.h>
+#pragma warning (disable :4996)
+#include "stack.h"
+#include "queue.h"
+
+void InsertSort(int *arr, int size);//插入排序
+void Print(int *arr, int size);
+void ShellSort(int *arr, int size);
+void Swap(int *num1, int *num2);
+void SelectSort(int *arr, int size);
+
+void HeadCreat(int *arr, int size);//建堆
+void ShiftDown(int *arr, int parent, int size);
+void HeapSort(int *arr, int size);//堆排
+void BubbleSort(int *arr, int size);//冒泡排序
+
+int partion(int *arr, int begin, int end);//hoare
+void QuickSort(int *arr, int begin, int end);//快排
+int partion2(int *arr, int begin, int end);//挖坑法
+int partion3(int *arr, int begin, int end);//双指针法
+int Getmid(int *arr, int begin, int end);//三数取中
+void QuickSortNoRSt(int *arr, int begin, int end);//非递归，利用栈实现快排
+void QuickSortNoRQe(int *arr, int begin, int end);//非递归，利用队列实现快排
+
+void Merge(int *arr, int begin, int mid, int end, int *temp);//合并
+void MergeR(int *arr, int begin, int end, int *temp);//递归
+void MergeSort(int *arr, int begin, int end, int size);//归并排序
+#endif 
---
+++ Sort/sort/test.c
@@ -0,0 +1,102 @@
+#include"sort.h"
+
+void  test()
+{
+	srand((unsigned)time(NULL));
+	int size = 0;
+	scanf("%d", &size);
+	int *arr = (int *)malloc(sizeof(int)*size);
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand();
+	}
+
+	int *arr1 = (int *)malloc(sizeof(int)*size);
+	int *arr2 = (int *)malloc(sizeof(int)*size);
+	int *arr3 = (int *)malloc(sizeof(int)*size);
+	int *arr4 = (int *)malloc(sizeof(int)*size);
+	int *arr5 = (int *)malloc(sizeof(int)*size);
+	int *arr6 = (int *)malloc(sizeof(int)*size);
+	int *arr7 = (int *)malloc(sizeof(int)*size);
+	int *arr8 = (int *)malloc(sizeof(int)*size);
+	int *arr9 = (int *)malloc(sizeof(int)*size);
+
+
+	memcpy(arr1, arr, sizeof(int)*size);
+	memcpy(arr2, arr, sizeof(int)*size);
+	memcpy(arr3, arr, sizeof(int)*size);
+	memcpy(arr4, arr, sizeof(int)*size);
+	memcpy(arr5, arr, sizeof(int)*size);
+	memcpy(arr6, arr, sizeof(int)*size);
+	memcpy(arr7, arr, sizeof(int)*size);
+	memcpy(arr8, arr, sizeof(int)*size);
+	memcpy(arr9, arr, sizeof(int)*size);
+
+	int begin = clock();
+	InsertSort(arr1, size);
+	int end = clock();
+	printf("Insert :%d\n\n", end - begin);
+	//Print(arr1, size);
+
+	begin = clock();
+	ShellSort(arr2, size);
+	end = clock();
+	printf("Shell :%d\n\n", end - begin);
+	//Print(arr2, size);
+
+	begin = clock();
+	SelectSort(arr3, size);
+	end = clock();
+	printf("Select :%d\n\n", end - begin);
+	//Print(arr3, size);
+
+	begin = clock();
+	HeapSort(arr4, size);
+	end = clock();
+	printf("Heap :%d\n\n", end - begin);
+	//Print(arr4, size);
+
+	//begin = clock();
+	//BubbleSort(arr5, size);
+	//end = clock();
+	//printf("Bubble :%d\n\n", end - begin);
+	////Print(arr5, size);
+
+	begin = clock();
+	QuickSort(arr6, 0, size - 1);
+	end = clock();
+	printf("Quick :%d\n\n", end - begin);
+	//Print(arr6, size);
+
+
+	begin = clock();
+	QuickSortNoRSt(arr7, 0, size - 1);
+	end = clock();
+	printf("QuickNorSt :%d\n\n", end - begin);
+	Print(arr7, size);
+
+	begin = clock();
+	QuickSortNoRQe(arr8, 0, size - 1);
+	end = clock();
+	printf("QuickNorQe :%d\n\n", end - begin);
+	Print(arr8, size);
+
+	begin = clock();
+	MergeSort(arr9, 0, size - 1, size);
+	end = clock();
+	printf("Merge :%d\n\n", end - begin);
+	Print(arr9, size);
+}
+
+
+
+int main()
+{
+
+	test();
+	system("pause");
+	return 0;
+}
+
+
+
