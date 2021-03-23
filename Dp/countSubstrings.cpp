32e24dc0fd944155dc64edf6f635ab48e0249205
姚存浩 <997106826@qq.com>
2021-03-23 18:08:52 +0800

    最大正方形

--- Dp/countSubstrings.cpp
+++ Dp/countSubstrings.cpp
@@ -1,29 +1,40 @@
-class Solution {
-public:
-	int count = 0;//统计回文串变量
+221. 最大正方形
 
-	void GetString(string s, int begin, int end)
+int maximalSquare(char** matrix, int matrixSize, int* matrixColSize){
+
+	int line = *matrixColSize;
+
+
+	for (int i = 0; i<matrixSize; i++)//matrix char 类型，注意陷阱
 	{
-		while (begin >= 0 && end<s.size() && s[begin] == s[end])
+		for (int j = 0; j<line; j++)
 		{
-			count++;
-			begin--;
-			end++;
+			matrix[i][j] -= '0';//转换成数字
 		}
+	}
+	for (int i = 1; i<matrixSize; i++)//从1开始，是因为上和左边界为右下角的正方形最大边长就是自己
+	{
+		for (int j = 1; j<line; j++)
+		{
+			if (matrix[i][j] != 0)//自己不能为零
+			{
+				matrix[i][j] =
+					((matrix[i - 1][j]>matrix[i][j - 1] ? matrix[i][j - 1] : matrix[i - 1][j])
+	>matrix[i - 1][j - 1] ? matrix[i - 1][j - 1] :
+	(matrix[i - 1][j]>matrix[i][j - 1] ? matrix[i][j - 1] : matrix[i - 1][j])) + 1;
 
+			}
+		}
+	}
+	int max = 0;
+	for (int i = 0; i<matrixSize; i++)
+	{
+		for (int j = 0; j<line; j++)
+		{
+			max = matrix[i][j]>max ? matrix[i][j] : max;//找出最大的max
+		}
 	}
 
-	int countSubstrings(string s) {
-		//中心扩展法，中心点只能是1个点或者2个点
-		//因为，3个点可以由1个点扩展而来
-		//4个点可以由两个点扩展而来
+	return max*max;
 
-		for (int i = 0; i<s.size(); i++)
-		{
-			GetString(s, i, i);//1个中心点
-			GetString(s, i, i + 1);//两个中心点
-		}
-
-		return count;
-	}
-};
+}
